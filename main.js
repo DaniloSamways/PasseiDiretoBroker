1743ff13f2f25aa271059b453b40cbd02eaf3116
Henrique <henriquemoreira10fk@gmail.com>
2023-10-31 13:52:51 -0300

    fix: formatted code and enchanced some parts

--- main.js
+++ main.js
@@ -1,36 +1,29 @@
-var removedModal = false;
-var removedAds = false;
+let removedModal = false;
+let removedAds = false;
 
-function removerBlur() {
-  if (removedModal === false) {
-    const premiumModal = document.getElementsByClassName('v--modal-overlay scrollable');
-    if (premiumModal.length > 0) {
-      premiumModal[0].remove();
+function removeBlur() {
+  if (!removedModal) {
+    const premiumModal = document.querySelector('.v--modal-overlay.scrollable');
+    if (premiumModal) {
+      premiumModal.remove();
       removedModal = true;
     }
   }
-  if (removedAds === false) {
-    const ads = document.getElementsByClassName('mv-content-limitation-fake-page short-preview-version');
-    if (ads.length > 0) {
-      ads[0].remove();
+  
+  if (!removedAds) {
+    const ads = document.querySelector('.mv-content-limitation-fake-page.short-preview-version');
+    if (ads) {
+      ads.remove();
       removedAds = true;
     }
-
-  }
-  const elementos = document.querySelectorAll('[style*="filter: blur(10px)"]');
-  const elementos5 = document.querySelectorAll('[style*="filter: blur(5px)"]');
-  for (let i = 0; i < elementos.length; i++) {
-    let estilo = elementos[i].getAttribute('style');
-    estilo = estilo.replace('filter: blur(10px)', 'filter: blur(0px)');
-    elementos[i].setAttribute('style', estilo);
   }
 
-  for (let i = 0; i < elementos5.length; i++) {
-    let estilo = elementos5[i].getAttribute('style');
-    estilo = estilo.replace('filter: blur(5px)', 'filter: blur(0px)');
-    elementos5[i].setAttribute('style', estilo);
-  }
+  const elements = document.querySelectorAll('[style*="filter: blur"]');
+  elements.forEach(element => {
+    const style = element.getAttribute('style');
+    element.setAttribute('style', style.replace(/filter: blur\(\d+px\)/, 'filter: blur(0px)'));
+  });
 }
-window.addEventListener('scroll', removerBlur);
 
-setInterval(removerBlur, 100)
+window.addEventListener('scroll', removeBlur);
+setInterval(removeBlur, 100);
